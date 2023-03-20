de2399abf5b9b25601b6552a41145c71ea105821
byeong <qudcks4885@gmail.com>
2023-03-20 10:51:05 +0900

    codeforces 160a greedy

--- CodeForces/160a.cpp
+++ CodeForces/160a.cpp
@@ -5,8 +5,28 @@
 
 using namespace std;
 
-int main(){
-
-
-  return 0;
+int main()
+{
+    int n , total = 0;
+    cin >> n;
+    vector<int> a(n);
+    for(int i =0; i < n;i++){
+        cin >> a[i];
+        total += a[i];
+    }
+    
+    sort(a.begin() , a.end(), greater<int>());
+    
+    int tmp = 0 ;
+    int i;
+    for(i =0; i < n; i++){
+        tmp += a[i];
+        total -= a[i];
+        if(tmp > total) break;
+    }
+    
+    cout << i+1 <<"\n";
+    
+    
+    return 0;
 }
