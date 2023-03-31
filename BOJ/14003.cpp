4322b47a40691d4ed0c0f605f9e551b7b9e039f8
byeong <qudcks4885@gmail.com>
2023-03-31 14:33:03 +0900

    Dp배열에 들어가는 idx 저장

--- BOJ/14003.cpp
+++ BOJ/14003.cpp
@@ -5,12 +5,14 @@
 
 using namespace std;
 
+
 int main()
 {
   int n , ans = 0;
   cin >> n;
   
   vector<int> a(n);
+  vector<int> id;
   vector<int> dp;
 
   dp.push_back(-1000000001);
@@ -19,21 +21,45 @@ int main()
     cin >> a[i];
   
   for(int i = 0; i < n; i++){
-    if(a[i] > dp.back()){
-      ans++;
-      dp.push_back(a[i]);
+      // auto iter = lower_bound(dp.begin(),dp.end(),a[i]);
+      // *iter = a[i];
+      //iterator -> index
+      auto idx = lower_bound(dp.begin(),dp.end(),a[i]) - dp.begin();
+      if(idx == dp.size()){
+        ans++;
+        dp.push_back(a[i]);
+      }
+      dp[idx] = a[i];
+      id.push_back(idx);
     }
-    else{
-      auto iter = lower_bound(dp.begin(),dp.end(),a[i]);
-      if(iter == prev(dp.end())) { *iter = a[i]; } 
+  
+
+  cout << ans << "\n";
+
+  //실제 배열 출력
+
+  // for(int i=1 ;i<=n;i++){
+  //   cout << dp[i] <<" ";
+  // }
+  // cout << "\n";
+
+  // for(int& e : id)
+  //   cout << e <<" ";
+
+  // cout << "\n";
+  stack<int> buf;
+  for(int i = n - 1; i >= 0; i--){
+    if(id[i] == ans){
+      buf.push(a[i]);
+      //cout << a[i] <<" ";
+      ans-=1;
     }
   }
 
-  cout << ans << "\n";
-  //실제 배열 출력
-  for(int i = 1; i<=ans; i++)
-    cout << dp[i] <<" ";
-  cout <<"\n";
+  while(!buf.empty()){
+    cout << buf.top() <<" ";
+    buf.pop();
+  }
 
   return 0;
 }
